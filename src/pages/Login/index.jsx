3eaf6b0aff9e03119cd0ab2f1d3c7de09175d6da
Mukesh Kumar <mukesh.scroffice@gmail.com>
2025-11-12 14:57:34 +0530

    no changes

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -106,7 +106,7 @@ const Login = () => {
 
         } catch (error) {
             console.error("Error during login:", error);
-            toast.error(error.message || "USER NOT FOUND! An error occurred during login. Please try again.");
+            toast.error(error.message || "USER NOT FOUND! An error occurred during login. Please try again..");
             reset();
         }
     };
